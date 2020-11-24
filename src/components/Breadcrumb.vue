3b0654971d0cf5f90d6a58d5a1758626e8088004
Furkan Bayram <hello@sfb.dev>
2020-11-25 02:25:02 +0300

    breadcrumb was updated.

--- src/components/Breadcrumb.vue
+++ src/components/Breadcrumb.vue
@@ -1,19 +1,25 @@
 <template>
   <ul class="breadcrumb bg-white mt-10px mb-0 p-0">
-    <li class="pr-1">
-      <a class="text-gray" href="#">Ciceksepeti Market > </a>
+    <li class="d-flex align-items-center">
+      <span class="text-gray pr-2">Çiçeksepeti Market</span>
+      <img src="../../src/assets/images/icon/right-arrow.svg" class="pl-1 pr-2" alt="Breadcrumb Arrow" />
     </li>
-    <li class="pr-1">
-      <a class="text-gray" href="#">İstanbul ></a>
+    <li class="d-flex align-items-center">
+      <a class="text-gray" href="#">
+        <span class="text-gray pr-1">İstanbul</span>
+        <img src="../../src/assets/images/icon/right-arrow.svg" class="pl-1 pr-2" alt="Breadcrumb Arrow" />
+      </a>
     </li>
-    <li class="pr-1">
-      <a class="text-secondary" href="">CiceksepetiBreadCrumb</a>
+    <li>
+      <a href="">
+        <span class="text-secondary">CiceksepetiBreadCrumb</span>
+      </a>
     </li>
   </ul>
 </template>
 
 <script>
-'breadcrumb';
+"breadcrumb";
 
 export default {
   data() {
